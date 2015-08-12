21e900b12db2894a515ec57299b000c7cd5acc7f
matheuspoleza <matheus.poleza@contaazul.com>
2015-08-12 12:22:42 -0300

    Added better way to get usarname

--- src/inbox.js
+++ src/inbox.js
@@ -3,10 +3,8 @@
 
 	var Inbox = function(){
 		this.getEmail = function(){
-			var user = window.window.BT_WorkerInitMessage;
-			var findMail = /[a-z0-9._%+-]+@+[a-z0-9._%+-]+.[a-z]{2,4}/g;
-			var mail = user.match(findMail);
-			return mail[0];
+			var userInfo = window.BT_SharedWorkerName.split('#');
+			return userInfo[1];
 		};
 	};
 
