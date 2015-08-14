0fa75b5627285be1c892208650e65521bef4849c
Randal Maia <randalmaia@gmail.com>
2015-08-13 23:57:10 -0300

    Get Username function and tests

--- src/inbox.js
+++ src/inbox.js
@@ -6,9 +6,11 @@
 			var userInfo = window.BT_SharedWorkerName.split('#');
 			return userInfo[1];
 		};
-	};
 
-	
+		this.getUsername = function(){
+			return document.querySelector('.gb_Xa').innerHTML;
+		};
+	};
 	
 	window.Inbox = Inbox;
 	
--- test/inbox.spec.js
+++ test/inbox.spec.js
@@ -1,7 +1,7 @@
 ;(function( should, sum, undefined ) {
   'use strict';
 
-  describe( 'User', function() {
+  describe( 'Get email', function() {
     beforeEach(function() {
 		window.BT_SharedWorkerName = 'shared_sync_worker#randalmaia@gmail.com';
 	});
@@ -10,6 +10,23 @@
 		var inbox = new Inbox();
 		var email = inbox.getEmail();
 		email.should.equal('randalmaia@gmail.com');
+	});
+  });
+
+  describe( 'Get Username', function() {
+	var nameContainer;
+    beforeEach(function() {
+		nameContainer= document.createElement('div');
+		nameContainer.setAttribute('class','gb_Xa');
+		nameContainer.innerHTML = "Randal Maia";
+		document.body.appendChild(nameContainer);
+	});
+
+	it("get user name", function () {
+		var inbox = new Inbox();
+		var username = inbox.getUsername();
+		console.log( document.querySelector('.gb_Xa').innerHTML);
+		username.should.equal('Randal Maia');
 	});
   });
 })( chai.should(), window.sum );
